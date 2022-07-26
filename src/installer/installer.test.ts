c6e29f608ed0f44f35e8d1b404a4956391b4d0e8
JesmoDev <jjensenmoeller@gmail.com>
2022-07-26 15:24:53 +0200

    added tests for installer components

--- src/installer/installer.test.ts
+++ src/installer/installer.test.ts
@@ -38,4 +38,66 @@ describe('UmbInstallerLayout', () => {
   });
 });
 
-//TODO: find a way to test the other elements: user, database, consent and installing.
+describe('UmbInstallerUser', () => {
+  let element: UmbInstallerUser;
+
+  beforeEach(async () => {
+    element = await fixture(html`<umb-installer-user></umb-installer-user>`);
+  });
+
+  it('is defined with its own instance', async () => {
+    expect(element).to.be.instanceOf(UmbInstallerUser);
+  });
+
+  it('passes the a11y audit', async () => {
+    expect(element).shadowDom.to.be.accessible();
+  });
+});
+
+describe('UmbInstallerConsent', () => {
+  let element: UmbInstallerConsent;
+
+  beforeEach(async () => {
+    element = await fixture(html`<umb-installer-consent></umb-installer-consent>`);
+  });
+
+  it('is defined with its own instance', async () => {
+    expect(element).to.be.instanceOf(UmbInstallerConsent);
+  });
+
+  it('passes the a11y audit', async () => {
+    expect(element).shadowDom.to.be.accessible();
+  });
+});
+
+describe('UmbInstallerDatabase', () => {
+  let element: UmbInstallerDatabase;
+
+  beforeEach(async () => {
+    element = await fixture(html`<umb-installer-database></umb-installer-database>`);
+  });
+
+  it('is defined with its own instance', async () => {
+    expect(element).to.be.instanceOf(UmbInstallerDatabase);
+  });
+
+  it('passes the a11y audit', async () => {
+    expect(element).shadowDom.to.be.accessible();
+  });
+});
+
+describe('UmbInstallerInstalling', () => {
+  let element: UmbInstallerInstalling;
+
+  beforeEach(async () => {
+    element = await fixture(html`<umb-installer-installing></umb-installer-installing>`);
+  });
+
+  it('is defined with its own instance', async () => {
+    expect(element).to.be.instanceOf(UmbInstallerInstalling);
+  });
+
+  it('passes the a11y audit', async () => {
+    expect(element).shadowDom.to.be.accessible();
+  });
+});
