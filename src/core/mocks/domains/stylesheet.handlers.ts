9f2c38a1f4dba56c04cdce4e3435fc227c92d1d2
Mads Rasmussen <madsr@hey.com>
2023-01-18 12:40:16 +0100

    add request handler for fetching a stylesheet

--- src/core/mocks/domains/stylesheet.handlers.ts
+++ src/core/mocks/domains/stylesheet.handlers.ts
@@ -3,6 +3,13 @@ import { umbStylesheetData } from '../data/stylesheet.data';
 
 // TODO: add schema
 export const handlers = [
+	rest.get('/umbraco/management/api/v1/tree/stylesheet/details/:path', (req, res, ctx) => {
+		const path = req.params.path as string;
+		if (!path) return;
+		const stylesheet = umbStylesheetData.getByPath(path);
+		return res(ctx.status(200), ctx.json(stylesheet));
+	}),
+
 	rest.get('/umbraco/management/api/v1/tree/stylesheet/root', (req, res, ctx) => {
 		const data = umbStylesheetData.getTreeRoot();
 		return res(ctx.status(200), ctx.json(data));
