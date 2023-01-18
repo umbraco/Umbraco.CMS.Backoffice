9b73116de1ad78d9b8be5983bcea294644bef039
Mads Rasmussen <madsr@hey.com>
2023-01-18 12:42:43 +0100

    add tree interface + helpers

---
+++ src/backoffice/shared/components/tree/index.ts
@@ -0,0 +1,49 @@
+import { v4 as uuid } from 'uuid';
+
+export interface UmbTreeItem {
+	unique: string;
+	parentUnique: string | null;
+	name?: string | null;
+	type?: string | null;
+	icon?: string | null;
+	hasChildren?: boolean;
+	isContainer?: boolean;
+	noAccess?: boolean;
+	isProtected?: boolean;
+	isPublished?: boolean;
+	isEdited?: boolean;
+	isElement?: boolean;
+	isFolder?: boolean;
+	isTrashed?: boolean;
+}
+
+export const createTreeItem = (
+	unique: string | null | undefined,
+	parentUnique: string | null | undefined,
+	data: Partial<UmbTreeItem>
+): UmbTreeItem => {
+	return {
+		unique: unique || uuid(),
+		parentUnique: parentUnique || null,
+		name: data.name,
+		type: data.type,
+		icon: data.icon,
+		hasChildren: data.hasChildren,
+		isContainer: data.isContainer,
+		noAccess: data.noAccess,
+		isProtected: data.isProtected,
+		isPublished: data.isPublished,
+		isEdited: data.isEdited,
+		isElement: data.isElement,
+		isFolder: data.isFolder,
+		isTrashed: data.isTrashed,
+	};
+};
+
+export const isTreeItem = (data: unknown): data is UmbTreeItem => {
+	return (
+		(data as UmbTreeItem).unique !== undefined &&
+		(data as UmbTreeItem).parentUnique !== undefined &&
+		(data as UmbTreeItem).hasChildren !== undefined
+	);
+};
