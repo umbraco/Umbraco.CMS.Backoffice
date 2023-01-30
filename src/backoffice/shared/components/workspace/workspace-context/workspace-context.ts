2eea0fb144f59a763d0febef4d027c267fa6a4ac
Niels Lyngsø <niels.lyngso@gmail.com>
2023-01-30 16:36:06 +0100

    note on workspace context alias

--- src/backoffice/shared/components/workspace/workspace-context/workspace-context.ts
+++ src/backoffice/shared/components/workspace/workspace-context/workspace-context.ts
@@ -1,6 +1,11 @@
 import { UmbContextProviderController } from '@umbraco-cms/context-api';
 import { UmbControllerHostInterface } from '@umbraco-cms/controller';
 
+/*
+
+TODO: We need to figure out if we like to keep using same alias for all workspace contexts.
+If so we need to align on a interface that all of these implements. otherwise consumers cant trust the workspace-context.
+*/
 export abstract class UmbWorkspaceContext {
 
 	protected _host: UmbControllerHostInterface;
