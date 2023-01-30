29a78b176a6df67b9a3ccfdfd5b48cfa641850d4
Niels Lyngsø <niels.lyngso@gmail.com>
2023-01-30 16:13:14 +0100

    correct alias

--- src/backoffice/shared/components/workspace/workspace-context/workspace-context.ts
+++ src/backoffice/shared/components/workspace/workspace-context/workspace-context.ts
@@ -1,14 +1,13 @@
 import { UmbContextProviderController } from '@umbraco-cms/context-api';
 import { UmbControllerHostInterface } from '@umbraco-cms/controller';
 
-
 export abstract class UmbWorkspaceContext {
 
 	protected _host: UmbControllerHostInterface;
 
 	constructor(host: UmbControllerHostInterface) {
 		this._host = host;
-		new UmbContextProviderController(host, 'UmbWorkspaceContext', this);
+		new UmbContextProviderController(host, 'umbWorkspaceContext', this);
 	}
 
 }
