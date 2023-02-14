bc61670ce0497d45a829a7bb100aad30a28f4998
Nathan Woulfe <nathan@nathanw.com.au>
2023-02-14 11:40:17 +1000

    use tree store base

--- src/backoffice/translation/dictionary/repository/dictionary.tree.store.ts
+++ src/backoffice/translation/dictionary/repository/dictionary.tree.store.ts
@@ -1,18 +1,14 @@
 import { UmbContextToken } from '@umbraco-cms/context-api';
-import { ArrayState } from '@umbraco-cms/observable-api';
-import { UmbStoreBase } from '@umbraco-cms/store';
+import { UmbTreeStoreBase } from '@umbraco-cms/store';
 import { UmbControllerHostInterface } from '@umbraco-cms/controller';
-import { EntityTreeItemModel } from '@umbraco-cms/backend-api';
 
 /**
  * @export
  * @class UmbDictionaryTreeStore
- * @extends {UmbStoreBase}
+ * @extends {UmbTreeStoreBase}
  * @description - Tree Data Store for Data Types
  */
-export class UmbDictionaryTreeStore extends UmbStoreBase {
-	#data = new ArrayState<EntityTreeItemModel>([], (x) => x.key);
-
+export class UmbDictionaryTreeStore extends UmbTreeStoreBase {
 	
 	/**
 	 * Creates an instance of UmbDictionaryTreeStore.
@@ -21,70 +17,6 @@ export class UmbDictionaryTreeStore extends UmbStoreBase {
 	 */
 	constructor(host: UmbControllerHostInterface) {
 		super(host, UMB_DICTIONARY_TREE_STORE_CONTEXT_TOKEN.toString());
-	}
-
-	/**
-	 * Appends items to the store
-	 * @param {Array<EntityTreeItemModel>} items
-	 * @memberof UmbDictionaryTreeStore
-	 */
-	appendItems(items: Array<EntityTreeItemModel>) {
-		this.#data.append(items);
-	}
-
-	/**
-	 * Updates an item in the store
-	 * @param {string} key
-	 * @param {Partial<EntityTreeItemModel>} data
-	 * @memberof UmbDictionaryTreeStore
-	 */
-	updateItem(key: string, data: Partial<EntityTreeItemModel>) {
-		const entries = this.#data.getValue();
-		const entry = entries.find((entry) => entry.key === key);
-
-		if (entry) {
-			this.#data.appendOne({ ...entry, ...data });
-		}
-	}
-
-	/**
-	 * Removes an item from the store
-	 * @param {string} key
-	 * @memberof UmbDictionaryTreeStore
-	 */
-	removeItem(key: string) {
-		const entries = this.#data.getValue();
-		const entry = entries.find((entry) => entry.key === key);
-
-		if (entry) {
-			this.#data.remove([key]);
-		}
-	}
-
-	/**
-	 * An observable to observe the root items
-	 * @memberof UmbDictionarTreeStore
-	 */
-	rootItems = this.#data.getObservablePart((items) => items.filter((item) => item.parentKey === null));
-
-	/**
-	 * Returns an observable to observe the children of a given parent
-	 * @param {(string | null)} parentKey
-	 * @return {*}
-	 * @memberof UmbDictionaryTreeStore
-	 */
-	childrenOf(parentKey: string | null) {
-		return this.#data.getObservablePart((items) => items.filter((item) => item.parentKey === parentKey));
-	}
-
-	/**
-	 * Returns an observable to observe the items with the given keys
-	 * @param {Array<string>} keys
-	 * @return {*}
-	 * @memberof UmbDictionaryTreeStore
-	 */
-	items(keys: Array<string>) {
-		return this.#data.getObservablePart((items) => items.filter((item) => keys.includes(item.key ?? '')));
 	}
 }
 
