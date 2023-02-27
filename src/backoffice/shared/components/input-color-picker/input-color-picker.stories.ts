942424cc773787585011522976e707a1efbd83fd
Warren Buckley <warren@umbraco.com>
2023-02-27 14:05:29 +0000

    Write story as CSF3 format

--- src/backoffice/shared/components/input-color-picker/input-color-picker.stories.ts
+++ src/backoffice/shared/components/input-color-picker/input-color-picker.stories.ts
@@ -1,62 +1,65 @@
-import { Meta, Story } from '@storybook/web-components';
-import { html } from 'lit-html';
-
-import type { UmbInputColorPickerElement } from './input-color-picker.element';
+import { Meta, StoryObj } from '@storybook/web-components';
 import './input-color-picker.element';
+import type { UmbInputColorPickerElement } from './input-color-picker.element';
 
-export default {
-	title: 'Components/Color Picker',
-	component: 'umb-input-color-picker',
-	id: 'umb-input-color-picker',
-} as Meta;
-
-
-const Template: Story<UmbInputColorPickerElement> = (args) => 
-    html`<umb-input-color-picker .swatches=${args.swatches} .showLabels=${args.showLabels} .value=${args.value}></umb-input-color-picker>`;
+const meta: Meta<UmbInputColorPickerElement> = {
+    title: 'Components/Color Picker',
+    component: 'umb-input-color-picker'
+};
   
-  export const Overview = Template.bind({});
-  Overview.args = {
-    showLabels: true,
-    swatches: [
-        {
-            label: "Red",
-            value: "#ff0000"
-        },
-        {
-            label: "Green",
-            value: "#00ff00"
-        }
-    ]
-  };
+export default meta;
+type Story = StoryObj<UmbInputColorPickerElement>;
   
-  export const WithoutLabels = Template.bind({});
-  WithoutLabels.args = {
-    showLabels: false,
-    swatches: [
-        {
-            label: "Red",
-            value: "#ff0000"
-        },
-        {
-            label: "Green",
-            value: "#00ff00"
-        }
-    ]
-  };
+export const Overview: Story = {
+    decorators: [],
+    args: {
+        showLabels: true,
+        swatches: [
+            {
+                label: "Red",
+                value: "#ff0000"
+            },
+            {
+                label: "Green",
+                value: "#00ff00"
+            }
+        ]
+    }
+};
 
-  // TODO: This doesn't check the correct swatch when the value is set
-  export const WithValueSet = Template.bind({});
-  WithValueSet.args = {
-    showLabels: true,
-    swatches: [
-        {
-            label: "Red",
-            value: "#ff0000"
-        },
-        {
-            label: "Green",
-            value: "#00ff00"
-        }
-    ],
-    value: "#00ff00"
-  };
+export const WithoutLabels: Story = {
+    decorators: [],
+    args: {
+        showLabels: false,
+        swatches: [
+            {
+                label: "Red",
+                value: "#ff0000"
+            },
+            {
+                label: "Green",
+                value: "#00ff00"
+            }
+        ]
+    }
+};
+
+// TODO: This doesn't check the correct swatch when the value is set
+// Perhaps a BUG ?
+export const WithValueLabels: Story = {
+    decorators: [],
+    args: {
+        showLabels: true,
+        swatches: [
+            {
+                label: "Red",
+                value: "#ff0000"
+            },
+            {
+                label: "Green",
+                value: "#00ff00"
+            }
+        ],
+        value: "#00ff00"
+    }
+};
