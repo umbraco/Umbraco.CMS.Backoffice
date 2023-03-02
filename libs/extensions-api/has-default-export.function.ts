687ba4fee95e5f495f0bb9821d1e55070cda4d6e
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-03-02 11:36:02 +0100

    reverse order of checks

--- libs/extensions-api/has-default-export.function.ts
+++ libs/extensions-api/has-default-export.function.ts
@@ -1,3 +1,3 @@
 export function hasDefaultExport<ConstructorType>(object: unknown): object is { default: ConstructorType } {
-	return typeof object === 'object' && object !== null && 'default' in object;
+	return object !== null && typeof object === 'object' && 'default' in object;
 }
