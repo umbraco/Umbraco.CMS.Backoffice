c9e80a4222da6471614816a6931e4c388ca9ce53
Julia Gru <56249914+julczka@users.noreply.github.com>
2023-03-10 13:11:24 +0100

    add markdown example and fancier template

--- src/backoffice/shared/components/code-editor/code-editor.stories.ts
+++ src/backoffice/shared/components/code-editor/code-editor.stories.ts
@@ -65,7 +65,39 @@ const codeSnippets: Record<CodeEditorLanguage, string> = {
 			@await Html.GetBlockGridItemAreaHtmlAsync(area)
 		}
 	</div>`,
-	markdown: ``,
+	markdown: `
+	You will like those projects!
+	
+	---
+	
+	# h1 Heading 8-)
+	## h2 Heading
+	### h3 Heading
+	#### h4 Heading
+	##### h5 Heading
+	###### h6 Heading
+	
+	
+	## Horizontal Rules
+	
+	___
+	
+	---
+	
+	***
+	
+	
+	## Typographic replacements
+	
+	Enable typographer option to see result.
+	
+	(c) (C) (r) (R) (tm) (TM) (p) (P) +-
+	
+	test.. test... test..... test?..... test!....
+	
+	!!!!!! ???? ,,  -- ---
+	
+	"Smartypants, double quotes" and 'single quotes'`,
 	typescript: `import { UmbTemplateRepository } from '../repository/template.repository';
 	import { UmbWorkspaceContext } from '../../../shared/components/workspace/workspace-context/workspace-context';
 	import { createObservablePart, DeepState } from '@umbraco-cms/observable-api';
