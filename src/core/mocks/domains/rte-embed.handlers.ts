48ac509d3651fd50121e258bd9bf044d908ade23
Nathan Woulfe <nathan@nathanw.com.au>
2023-03-13 17:01:34 +1000

    moves modal to shared/modals updates with token fixes story clean up data handling to only pass required

--- src/core/mocks/domains/rte-embed.handlers.ts
+++ src/core/mocks/domains/rte-embed.handlers.ts
@@ -1,13 +1,13 @@
 import { rest } from "msw";
+import { OEmbedResult, OEmbedStatus } from "../../../backoffice/shared/modals/embedded-media";
 import { umbracoPath } from "@umbraco-cms/utils";
-import { OEmbedResult, OEmbedStatus } from "src/core/modal/layouts/embedded-media/modal-layout-embedded-media.element";
 
 export const handlers = [
 	rest.get(umbracoPath('/rteembed'), (req, res, ctx) => {
 		const width = req.url.searchParams.get('width') ?? 360;
 		const height = req.url.searchParams.get('height') ?? 240;
 		const response: OEmbedResult = {
-			supportsDimensions: true,
+			supportsDimensions: true,			
 			markup: `<iframe width="${width}" height="${height}" src="https://www.youtube.com/embed/wJNbtYdr-Hg?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen title="Sleep Token - The Summoning"></iframe>`,
 			oEmbedStatus: OEmbedStatus.Success,			
 		};
