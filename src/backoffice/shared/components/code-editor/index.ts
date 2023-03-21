40f69b4f91d4b797c3ea4247a88931629dc15730
Julia Gru <56249914+julczka@users.noreply.github.com>
2023-03-21 11:32:56 +0100

    test heap size fix

--- src/backoffice/shared/components/code-editor/index.ts
+++ src/backoffice/shared/components/code-editor/index.ts
@@ -1,7 +1,8 @@
 import * as initializeWorkers from './languageWorkers';
-
 import { UmbCodeEditorElement } from './code-editor.element';
+import { UmbCodeEditor } from './code-editor';
+import { monacoEditorStyles } from './styles';
 
 export default UmbCodeEditorElement;
 
-export { initializeWorkers };
+export { initializeWorkers, UmbCodeEditor, UmbCodeEditorElement, monacoEditorStyles };
