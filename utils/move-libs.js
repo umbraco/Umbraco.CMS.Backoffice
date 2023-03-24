259a549ec2e36169563c79c961e7ce346f1c1873
Warren Buckley <warren@umbraco.com>
2023-03-24 09:33:24 +0000

    Copied file from wrong location as they are being generated and put at the root of the main project

--- utils/move-libs.js
+++ utils/move-libs.js
@@ -15,6 +15,7 @@
 
 import { readdirSync, readFileSync, writeFileSync, cpSync, mkdirSync } from 'fs';
 
+const rootDir = './';
 const srcDir = './libs';
 const inputDir = './dist/libs';
 const outputDir = '../Umbraco.Cms.StaticAssets/wwwroot/umbraco/backoffice/libs';
@@ -22,8 +23,8 @@ const outputDir = '../Umbraco.Cms.StaticAssets/wwwroot/umbraco/backoffice/libs';
 // Copy package files
 cpSync(`${srcDir}/package.json`, `${inputDir}/package.json`, { recursive: true });
 cpSync(`${srcDir}/README.md`, `${inputDir}/README.md`, { recursive: true });
-cpSync(`${srcDir}/custom-elements.json`, `${inputDir}/custom-elements.json`, { recursive: true });
-cpSync(`${srcDir}/vscode-html-custom-data.json`, `${inputDir}/vscode-html-custom-data.json`, { recursive: true });
+cpSync(`${rootDir}/custom-elements.json`, `${inputDir}/custom-elements.json`, { recursive: true });
+cpSync(`${rootDir}/vscode-html-custom-data.json`, `${inputDir}/vscode-html-custom-data.json`, { recursive: true });
 
 const libs = readdirSync(inputDir);
 
