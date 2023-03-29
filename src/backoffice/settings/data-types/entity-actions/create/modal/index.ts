e0dbe2a9311fbeef9d5b169208c9e95d612127b9
Mads Rasmussen <madsr@hey.com>
2023-03-29 15:42:52 +0200

    add token

--- src/backoffice/settings/data-types/entity-actions/create/modal/index.ts
+++ src/backoffice/settings/data-types/entity-actions/create/modal/index.ts
@@ -1,6 +1,11 @@
 import { UmbModalToken } from '@umbraco-cms/backoffice/modal';
 
-export const UMB_CREATE_DATA_TYPE_MODAL_TOKEN = new UmbModalToken('Umb.Modal.CreateDataType', {
+export const UMB_CREATE_DATA_TYPE_MODAL = new UmbModalToken('Umb.Modal.CreateDataType', {
+	type: 'sidebar',
+	size: 'small',
+});
+
+export const UMB_CREATE_DATA_TYPE_FOLDER_MODAL = new UmbModalToken('Umb.Modal.CreateDataTypeFolder', {
 	type: 'sidebar',
 	size: 'small',
 });
