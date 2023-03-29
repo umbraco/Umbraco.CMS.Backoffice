a1370c19f361c47d05f6ee705a83ee0248e671af
Mads Rasmussen <madsr@hey.com>
2023-03-29 15:41:47 +0200

    create interface for folder repository

---
+++ libs/repository/folder-repository.interface.ts
@@ -0,0 +1,24 @@
+import type {
+	CreateFolderRequestModel,
+	FolderReponseModel,
+	ProblemDetailsModel,
+} from '@umbraco-cms/backoffice/backend-api';
+
+export interface UmbFolderRepository<
+	RequestType extends CreateFolderRequestModel,
+	ResponseType extends FolderReponseModel
+> {
+	createFolder(folder: RequestType): Promise<{
+		data?: string;
+		error?: ProblemDetailsModel;
+	}>;
+
+	requestFolder(unique: string): Promise<{
+		data?: ResponseType;
+		error?: ProblemDetailsModel;
+	}>;
+
+	deleteFolder(key: string): Promise<{
+		error?: ProblemDetailsModel;
+	}>;
+}
