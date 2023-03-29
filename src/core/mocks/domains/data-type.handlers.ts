9dcbc18b49c850bbdc41c5d546647ea2fe11698c
Mads Rasmussen <madsr@hey.com>
2023-03-29 12:50:53 +0200

    update data type handlers

--- src/core/mocks/domains/data-type.handlers.ts
+++ src/core/mocks/domains/data-type.handlers.ts
@@ -4,43 +4,13 @@ import { umbracoPath } from '@umbraco-cms/backoffice/utils';
 
 // TODO: add schema
 export const handlers = [
-	rest.delete<string[]>('/umbraco/backoffice/data-type/:key', async (req, res, ctx) => {
+	rest.delete<string>(umbracoPath('/data-type/:key'), async (req, res, ctx) => {
 		const key = req.params.key as string;
 		if (!key) return;
 
 		umbDataTypeData.delete([key]);
 
 		return res(ctx.status(200));
-	}),
-
-	rest.get('/umbraco/management/api/v1/tree/data-type/root', (req, res, ctx) => {
-		const rootItems = umbDataTypeData.getTreeRoot();
-		const response = {
-			total: rootItems.length,
-			items: rootItems,
-		};
-		return res(ctx.status(200), ctx.json(response));
-	}),
-
-	rest.get('/umbraco/management/api/v1/tree/data-type/children', (req, res, ctx) => {
-		const parentKey = req.url.searchParams.get('parentKey');
-		if (!parentKey) return;
-
-		const children = umbDataTypeData.getTreeItemChildren(parentKey);
-
-		const response = {
-			total: children.length,
-			items: children,
-		};
-
-		return res(ctx.status(200), ctx.json(response));
-	}),
-
-	rest.get('/umbraco/management/api/v1/tree/data-type/item', (req, res, ctx) => {
-		const keys = req.url.searchParams.getAll('key');
-		if (!keys) return;
-		const items = umbDataTypeData.getTreeItem(keys);
-		return res(ctx.status(200), ctx.json(items));
 	}),
 
 	rest.get(umbracoPath('/data-type/:key'), (req, res, ctx) => {
@@ -68,5 +38,35 @@ export const handlers = [
 		const saved = umbDataTypeData.save(data);
 
 		return res(ctx.status(200), ctx.json(saved));
+	}),
+
+	rest.get(umbracoPath('/tree/data-type/root'), (req, res, ctx) => {
+		const rootItems = umbDataTypeData.getTreeRoot();
+		const response = {
+			total: rootItems.length,
+			items: rootItems,
+		};
+		return res(ctx.status(200), ctx.json(response));
+	}),
+
+	rest.get(umbracoPath('/tree/data-type/children'), (req, res, ctx) => {
+		const parentKey = req.url.searchParams.get('parentKey');
+		if (!parentKey) return;
+
+		const children = umbDataTypeData.getTreeItemChildren(parentKey);
+
+		const response = {
+			total: children.length,
+			items: children,
+		};
+
+		return res(ctx.status(200), ctx.json(response));
+	}),
+
+	rest.get(umbracoPath('/tree/data-type/item'), (req, res, ctx) => {
+		const keys = req.url.searchParams.getAll('key');
+		if (!keys) return;
+		const items = umbDataTypeData.getTreeItem(keys);
+		return res(ctx.status(200), ctx.json(items));
 	}),
 ];
