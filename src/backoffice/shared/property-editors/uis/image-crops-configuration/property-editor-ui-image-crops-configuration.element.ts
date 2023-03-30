ab72e533882cad4e52c880a658894d6d782b0222
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-03-30 13:31:23 +1300

    styling

--- src/backoffice/shared/property-editors/uis/image-crops-configuration/property-editor-ui-image-crops-configuration.element.ts
+++ src/backoffice/shared/property-editors/uis/image-crops-configuration/property-editor-ui-image-crops-configuration.element.ts
@@ -25,31 +25,45 @@ export class UmbPropertyEditorUIImageCropsConfigurationElement
 	public config = [];
 
 	@state()
-	private crops: Array<ImageCrop> = [];
+	private _crops: Array<ImageCrop> = [];
 
 	#addCrop() {
-		this.crops.push({
+		this._crops.push({
 			alias: '',
 			width: 0,
 			height: 0,
 		});
 
-		this.requestUpdate('crops');
+		this.requestUpdate('_crops');
+	}
+
+	#removeCrop(index: number) {
+		this._crops.splice(index, 1);
+
+		this.requestUpdate('_crops');
 	}
 
 	render() {
-		return this.crops.length > 0 ? this.#renderCrops() : this.#renderEmpty();
+		return this._crops.length > 0 ? this.#renderCrops() : this.#renderEmpty();
 	}
 
 	#renderCrops() {
-		return html`<div>
+		return html`<div id="crops">
 			<div class="crop">
-				<b>Alias</b>
+				<b style="grid-column: 2;">Alias</b>
 				<b>Width</b>
 				<b>Height</b>
 			</div>
-			${this.crops.map(
+			${this._crops.map(
 				(crop) => html`<div class="crop">
+					<div class="move-button">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
+							<path fill="none" d="M0 0h24v24H0z" />
+							<path
+								fill="currentColor"
+								d="M18 11V8l4 4-4 4v-3h-5v5h3l-4 4-4-4h3v-5H6v3l-4-4 4-4v3h5V6H8l4-4 4 4h-3v5z" />
+						</svg>
+					</div>
 					<uui-input style="border-right: none;" autocomplete="off">${crop.alias}</uui-input>
 					<uui-input style="border-right: none;" autocomplete="off">
 						${crop.width}
@@ -59,8 +73,12 @@ export class UmbPropertyEditorUIImageCropsConfigurationElement
 						${crop.height}
 						<div slot="append" class="crop-pixel">px</div>
 					</uui-input>
+					<uui-button color="danger" compact class="remove-button" @click=${this.#removeCrop} label="remove crop">
+						<uui-icon name="umb:trash"></uui-icon>
+					</uui-button>
 				</div>`
 			)}
+			<uui-button @click=${this.#addCrop} look="placeholder" label="Add crop"></uui-button>
 		</div>`;
 	}
 
@@ -74,7 +92,7 @@ export class UmbPropertyEditorUIImageCropsConfigurationElement
 				</p>
 			</div>
 			<div id="empty-state-actions">
-				<uui-button @click=${this.#addCrop} look="outline">Add crop</uui-button>
+				<uui-button @click=${this.#addCrop} look="outline" label="Add crop"></uui-button>
 				<a
 					target="_blank"
 					href="https://docs.umbraco.com/umbraco-cms/fundamentals/backoffice/property-editors/built-in-umbraco-property-editors/image-cropper">
@@ -88,8 +106,13 @@ export class UmbPropertyEditorUIImageCropsConfigurationElement
 	static styles = [
 		UUITextStyles,
 		css`
+			#crops {
+				display: flex;
+				flex-direction: column;
+				gap: var(--uui-size-space-2);
+			}
 			#empty-state-description {
-				padding-top: var(--uui-size-space-1);
+				margin-top: var(--uui-size-space-1);
 			}
 			#empty-state-actions {
 				display: flex;
@@ -100,9 +123,27 @@ export class UmbPropertyEditorUIImageCropsConfigurationElement
 				color: var(--uui-color-interactive);
 				text-decoration: none;
 			}
+			#empty-state-actions a:hover {
+				color: var(--uui-color-interactive-emphasis);
+			}
 			.crop {
 				display: grid;
-				grid-template-columns: 1fr 100px 100px;
+				grid-template-columns: var(--uui-size-11) 1fr var(--uui-size-32) var(--uui-size-32) var(--uui-size-11);
+			}
+			.move-button {
+				aspect-ratio: 1;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				opacity: 0.5;
+			}
+			.remove-button {
+				aspect-ratio: 1;
+				opacity: 0;
+			}
+			.crop:focus-within .remove-button,
+			.crop:hover .remove-button {
+				opacity: 1;
 			}
 			.crop-pixel {
 				background-color: var(--uui-color-disabled);
@@ -113,6 +154,7 @@ export class UmbPropertyEditorUIImageCropsConfigurationElement
 				align-items: center;
 				justify-content: center;
 				font-size: 0.8rem;
+				border-left: 1px solid var(--uui-color-divider-standalone);
 			}
 		`,
 	];
