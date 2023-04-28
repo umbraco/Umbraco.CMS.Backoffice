a1f87b8cb4957f9e38cfa4031d6a839291568c5a
Mads Rasmussen <madsr@hey.com>
2023-04-28 13:46:09 +0200

    fix duplicate import

--- src/backoffice/documents/document-types/workspace/document-type-workspace.context.ts
+++ src/backoffice/documents/document-types/workspace/document-type-workspace.context.ts
@@ -1,10 +1,6 @@
 import { UmbContentTypePropertyStructureManager } from '@umbraco-cms/backoffice/content-type';
 import { UmbDocumentTypeRepository } from '../repository/document-type.repository';
-import {
-	UmbEntityWorkspaceContextInterface,
-	UmbWorkspaceContext,
-	UmbEntityWorkspaceContextInterface,
-} from '@umbraco-cms/backoffice/workspace';
+import { UmbWorkspaceContext, UmbEntityWorkspaceContextInterface } from '@umbraco-cms/backoffice/workspace';
 import type {
 	ContentTypeCompositionModel,
 	ContentTypeSortModel,
