7b7d69e448bcff4dac4d0490d53c01facdc15050
Niels Lyngsø <niels.lyngso@gmail.com>
2023-10-03 13:10:03 +0200

    correct typings

--- src/libs/context-api/controller/base-context.controller.ts
+++ src/libs/context-api/controller/base-context.controller.ts
@@ -1,9 +1,9 @@
 import type { UmbContextToken } from "../token/index.js";
 import { UmbBaseController, UmbControllerHost } from "@umbraco-cms/backoffice/controller-api";
 
-export abstract class UmbBaseContext extends UmbBaseController {
-	constructor(host: UmbControllerHost, alias: string | UmbContextToken) {
+export abstract class UmbBaseContext<BaseType = any, ResultType extends BaseType = BaseType> extends UmbBaseController {
+	constructor(host: UmbControllerHost, alias: string | UmbContextToken<BaseType, ResultType> ) {
 		super(host);
-		this.provideContext(alias, this);
+		this.provideContext(alias, this as unknown as ResultType);
 	}
 }
