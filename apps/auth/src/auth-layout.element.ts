c8ec4ea0ce4a8dac8b45faedd0f643f33e8ab292
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-10-24 17:08:45 +1300

    layout wip

--- apps/auth/src/auth-layout.element.ts
+++ apps/auth/src/auth-layout.element.ts
@@ -5,32 +5,78 @@ import { customElement } from 'lit/decorators.js';
 export class UmbAuthLayoutElement extends LitElement {
 	render() {
 		return html`
-			<div id="background"></div>
+			<div id="layout">
+				<div id="image-column">
+					<div id="image"></div>
+				</div>
+				<div id="auth-column">
+					<div id="auth-box">
+						<slot></slot>
+					</div>
+				</div>
+			</div>
+			<!-- <div id="background"></div>
 
 			<div id="logo">
-				<img src="umbraco_logomark_white.svg" alt="Umbraco" />
+				<img src="umbraco_logo_white_horizontal.svg" alt="Umbraco" />
 			</div>
 
 			<div id="container">
+				<div>
+					<div id="image"></div>
+				</div>
 				<uui-box id="box">
 					<slot></slot>
 				</uui-box>
-			</div>
+			</div> -->
 		`;
 	}
 
 	static styles: CSSResultGroup = [
 		css`
-			#background {
-				position: fixed;
+			:host {
+				display: block;
+				height: 100dvh;
+				background-color: white;
+			}
+			#layout {
+				display: flex;
+				gap: 32px;
+				height: 100%;
+			}
+			#image-column {
+				max-width: 800px;
+				width: 100%;
+				overflow: hidden;
+				position: relative;
+				padding: 32px;
+				padding-right: 0;
+				box-sizing: border-box;
+			}
+			#auth-column {
+				width: 100%;
+				padding: 32px;
+				box-sizing: border-box;
+				min-width: 600px;
+			}
+			#image {
+				z-index: 0;
 				overflow: hidden;
 				background-position: 50%;
 				background-repeat: no-repeat;
 				background-size: cover;
-				background-image: url('login.jpeg');
-				width: 100vw;
-				height: 100vh;
+				background-image: url('loginImage.jpg');
+				width: 100%;
+				height: 100%;
+				transform: scaleX(-1);
+				border-radius: 32px;
 			}
+			#auth-box {
+				max-width: 300px;
+				box-sizing: border-box;
+				margin: 250px auto;
+			}
+			/* 
 
 			#logo {
 				position: fixed;
@@ -60,7 +106,7 @@ export class UmbAuthLayoutElement extends LitElement {
 			#email,
 			#password {
 				width: 100%;
-			}
+			} */
 		`,
 	];
 }
