8e32630558590111f9fff8c672e906936ed80d38
Mads Rasmussen <madsr@hey.com>
2024-03-13 08:52:46 +0100

    add entity type to item models

--- src/packages/data-type/repository/item/types.ts
+++ src/packages/data-type/repository/item/types.ts
@@ -1,5 +1,8 @@
+import type { UmbDataTypeEntityType } from '../../entity.js';
+
 export interface UmbDataTypeItemModel {
 	unique: string;
+	entityType: UmbDataTypeEntityType;
 	name: string;
 	propertyEditorUiAlias: string;
 }
--- src/packages/documents/document-types/repository/item/types.ts
+++ src/packages/documents/document-types/repository/item/types.ts
@@ -1,5 +1,8 @@
+import type { UmbDocumentTypeEntityType } from '../../entity.js';
+
 export type UmbDocumentTypeItemModel = {
 	unique: string;
+	entityType: UmbDocumentTypeEntityType;
 	name: string;
 	isElement: boolean;
 	icon?: string | null;
