d18b020c09de03128bf30c138a2a3c3ad54d4e0c
Mads Rasmussen <madsr@hey.com>
2024-04-19 13:00:41 +0200

    add manifest and umbraco-package to entries

--- src/packages/data-type/vite.config.ts
+++ src/packages/data-type/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	build: {
 		lib: {
-			entry: ['index.ts'],
+			entry: ['index.ts', 'manifests.ts', 'umbraco-package.ts'],
 			formats: ['es'],
 		},
 		outDir: 'dist',
