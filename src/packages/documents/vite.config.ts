d795dfd4010e74ab746f4ddad5a0aff1ce4dd26f
Mads Rasmussen <madsr@hey.com>
2024-04-19 13:36:39 +0200

    put index files in the same place

--- src/packages/documents/vite.config.ts
+++ src/packages/documents/vite.config.ts
@@ -6,9 +6,9 @@ export default defineConfig({
 			entry: {
 				manifest: 'manifests.ts',
 				'umbraco-package': 'umbraco-package.ts',
-				document: './documents/index.ts',
-				'document-type': './document-types/index.ts',
-				'document-blueprint': './document-blueprints/index.ts',
+				'documents/index': './documents/index.ts',
+				'document-types/index': './document-types/index.ts',
+				'document-blueprints/index': './document-blueprints/index.ts',
 			},
 			formats: ['es'],
 		},
