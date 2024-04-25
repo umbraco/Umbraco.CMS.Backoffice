623e25398b7950fdafd330ffecd4d7cb44049aa1
leekelleher <leekelleher@gmail.com>
2024-04-25 14:56:04 +0100

    MultiUrlPicker, validates the min/max config

--- src/packages/property-editors/multi-url-picker/property-editor-ui-multi-url-picker.element.ts
+++ src/packages/property-editors/multi-url-picker/property-editor-ui-multi-url-picker.element.ts
@@ -21,9 +21,15 @@ export class UmbPropertyEditorUIMultiUrlPickerElement extends UmbLitElement impl
 
 		this._hideAnchor = config.getValueByAlias('hideAnchor') ?? false;
 		this._ignoreUserStartNodes = config.getValueByAlias<boolean>('ignoreUserStartNodes') ?? false;
-		this._minNumber = Number(config.getValueByAlias('minNumber')) ?? 0;
-		this._maxNumber = Number(config.getValueByAlias('maxNumber')) ?? Infinity;
 		this._overlaySize = config.getValueByAlias<UUIModalSidebarSize>('overlaySize') ?? 'small';
+
+		this.min = config.getValueByAlias('minNumber', (value) => this.#parseIntOrFallback(value, 0)) ?? 0;
+		this.max = config.getValueByAlias('maxNumber', (value) => this.#parseIntOrFallback(value, Infinity)) ?? Infinity;
+	}
+
+	#parseIntOrFallback(input: unknown, fallback: number): number {
+		const num = Number(input);
+		return num > 0 ? num : fallback;
 	}
 
 	@state()
@@ -36,10 +42,10 @@ export class UmbPropertyEditorUIMultiUrlPickerElement extends UmbLitElement impl
 	private _ignoreUserStartNodes?: boolean;
 
 	@state()
-	private _minNumber? = 0;
+	min = 0;
 
 	@state()
-	private _maxNumber? = Infinity;
+	max = Infinity;
 
 	@state()
 	private _alias?: string;
@@ -66,8 +72,8 @@ export class UmbPropertyEditorUIMultiUrlPickerElement extends UmbLitElement impl
 			<umb-input-multi-url
 				.alias=${this._alias}
 				.ignoreUserStartNodes=${this._ignoreUserStartNodes}
-				.max=${this._maxNumber}
-				.min=${this._minNumber}
+				.min=${this.min}
+				.max=${this.max}
 				.overlaySize=${this._overlaySize}
 				.urls=${this.value ?? []}
 				.variantId=${this._variantId}
