ebd93ae54ed75686347ddf5386123dac5dd9878e
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-05-10 13:06:06 +0200

    fix: re-arrange colors and find matching UUI variables corresponding to the old backoffice
    note: there are a few variables that have no equivalent in UUI at least, but the browser has an alright fallback to those, except for "deep-purple" which is simply neither here nor there

--- src/packages/core/resources/extractUmbColorVariable.function.ts
+++ src/packages/core/resources/extractUmbColorVariable.function.ts
@@ -1,25 +1,25 @@
 export const umbracoColors = [
-	{ alias: 'text', varName: '--uui-color-text' },
-	{ alias: 'black', varName: '--uui-color-text' },
-	{ alias: 'yellow', varName: '--uui-palette-sunglow' },
-	{ alias: 'pink', varName: '--uui-palette-spanish-pink' },
-	{ alias: 'blue', varName: '--uui-palette-violet-blue' },
-	{ alias: 'light-blue', varName: '--uui-palette-malibu' },
-	{ alias: 'red', varName: '--uui-palette-maroon-flush' },
-	{ alias: 'green', varName: '--uui-palette-jungle-green' },
+	{ alias: 'black', varName: '--uui-palette-black' },
+	{ alias: 'blue-grey', varName: '--uui-palette-space-cadet-dimmed' },
+	{ alias: 'grey', varName: '--uui-palette-grey' },
 	{ alias: 'brown', varName: '--uui-palette-chamoisee' },
-	{ alias: 'grey', varName: '--uui-palette-dusty-grey' },
+	{ alias: 'blue', varName: '--uui-palette-malibu' },
+	{ alias: 'light-blue', varName: '--uui-palette-malibu-light' },
+	{ alias: 'indigo', varName: '--uui-palette-violet-blue' },
+	{ alias: 'purple', varName: '--uui-palette-purple' }, // Missing UUI variable
+	{ alias: 'deep-purple', varName: '--uui-palette-space-cadet' }, // Missing UUI variable
+	{ alias: 'cyan', varName: '--uui-palette-cyan' }, // Missing UUI variable
+	{ alias: 'green', varName: '--uui-palette-forest-green' },
+	{ alias: 'light-green', varName: '--uui-palette-jungle-green' },
+	{ alias: 'lime', varName: '--uui-palette-jungle-green-light' },
+	{ alias: 'yellow', varName: '--uui-palette-sunglow' },
+	{ alias: 'amber', varName: '--uui-palette-deep-saffron-light' },
+	{ alias: 'orange', varName: '--uui-palette-deep-saffron' },
+	{ alias: 'deep-orange', varName: '--uui-palette-deep-saffron-dark' },
+	{ alias: 'red', varName: '--uui-palette-maroon-flush' },
+	{ alias: 'pink', varName: '--uui-palette-spanish-pink' },
 
-	{ alias: 'blue-grey', legacy: true, varName: '--uui-palette-dusty-grey' },
-	{ alias: 'indigo', legacy: true, varName: '--uui-palette-malibu' },
-	{ alias: 'purple', legacy: true, varName: '--uui-palette-space-cadet' },
-	{ alias: 'deep-purple', legacy: true, varName: '--uui-palette-space-cadet' },
-	{ alias: 'cyan', legacy: true, varName: '-uui-palette-jungle-green' },
-	{ alias: 'light-green', legacy: true, varName: '-uui-palette-jungle-green' },
-	{ alias: 'lime', legacy: true, varName: '-uui-palette-jungle-green' },
-	{ alias: 'amber', legacy: true, varName: '--uui-palette-chamoisee' },
-	{ alias: 'orange', legacy: true, varName: '--uui-palette-chamoisee' },
-	{ alias: 'deep-orange', legacy: true, varName: '--uui-palette-cocoa-brown' },
+	{ alias: 'text', legacy: true, varName: '--uui-color-text' },
 ];
 
 export function extractUmbColorVariable(colorAlias: string): string | undefined {
