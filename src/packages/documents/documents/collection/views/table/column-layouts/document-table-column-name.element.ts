4f0cfa6bc31593961d9556fce46d48235d852ed9
Mads Rasmussen <madsr@hey.com>
2024-05-21 14:27:31 +0200

    render variant name

--- src/packages/documents/documents/collection/views/table/column-layouts/document-table-column-name.element.ts
+++ src/packages/documents/documents/collection/views/table/column-layouts/document-table-column-name.element.ts
@@ -1,8 +1,12 @@
 import type { UmbEditableDocumentCollectionItemModel } from '../../../types.js';
-import { css, customElement, html, nothing, property } from '@umbraco-cms/backoffice/external/lit';
+import { css, customElement, html, nothing, property, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import type { UmbTableColumn, UmbTableColumnLayoutElement, UmbTableItem } from '@umbraco-cms/backoffice/components';
 import type { UUIButtonElement } from '@umbraco-cms/backoffice/external/uui';
+import { UmbVariantId } from '@umbraco-cms/backoffice/variant';
+import { UMB_APP_LANGUAGE_CONTEXT, type UmbAppLanguageContext } from '@umbraco-cms/backoffice/language';
+import type { UmbVariantDatasetWorkspaceContext } from '@umbraco-cms/backoffice/workspace';
+import { UMB_VARIANT_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/workspace';
 
 @customElement('umb-document-table-column-name')
 export class UmbDocumentTableColumnNameElement extends UmbLitElement implements UmbTableColumnLayoutElement {
@@ -12,10 +16,62 @@ export class UmbDocumentTableColumnNameElement extends UmbLitElement implements 
 	@property({ attribute: false })
 	value!: UmbEditableDocumentCollectionItemModel;
 
+	@state()
+	_workspaceActiveVariantId?: UmbVariantId;
+
+	@state()
+	_appDefaultCulture?: string;
+
 	#onClick(event: Event & { target: UUIButtonElement }) {
 		event.preventDefault();
 		event.stopPropagation();
 		window.history.pushState(null, '', event.target.href);
+	}
+
+	#appLanguageContext?: UmbAppLanguageContext;
+	#workspaceContext?: UmbVariantDatasetWorkspaceContext;
+
+	constructor() {
+		super();
+
+		this.consumeContext(UMB_APP_LANGUAGE_CONTEXT, (instance) => {
+			this.#appLanguageContext = instance;
+			this.#observeDefaultCulture();
+		});
+
+		this.consumeContext(UMB_VARIANT_WORKSPACE_CONTEXT, (instance) => {
+			if (!instance) return;
+			this.#workspaceContext = instance;
+			this.#observeWorkspaceActiveVariant();
+		});
+	}
+
+	#observeDefaultCulture() {
+		this.observe(this.#appLanguageContext!.appDefaultLanguage, (value) => {
+			this._appDefaultCulture = value?.unique;
+		});
+	}
+
+	#observeWorkspaceActiveVariant() {
+		this.observe(
+			this.#workspaceContext?.splitView.activeVariantsInfo,
+			(value) => {
+				if (!value) return;
+				this._workspaceActiveVariantId = UmbVariantId.Create(value[0]);
+			},
+			'umbCollectionItemVariantNameObserver',
+		);
+	}
+
+	#getItemVariantName() {
+		const item = this.value.item;
+
+		const fallbackName =
+			item.variants.find((variant) => variant.culture === this._appDefaultCulture)?.name ??
+			item.variants[0].name ??
+			'Unknown';
+		const name = item.variants.find((variant) => this._workspaceActiveVariantId?.compare(variant))?.name;
+		return name ?? `(${fallbackName})`;
 	}
 
 	render() {
@@ -24,7 +80,7 @@ export class UmbDocumentTableColumnNameElement extends UmbLitElement implements 
 			<uui-button
 				compact
 				href=${this.value.editPath}
-				label=${this.value.item.name}
+				label=${this.#getItemVariantName()}
 				@click=${this.#onClick}></uui-button>
 		`;
 	}
