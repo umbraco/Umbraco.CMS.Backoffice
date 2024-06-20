ea15e43cc32885cc00dd980624979ee18acb67a5
Rick Butterfield <r.butterfield@wearewattle.com>
2024-06-20 08:51:01 +0100

    Fix whitespace issues

--- src/packages/block/block/workspace/block-workspace-editor.element.ts
+++ src/packages/block/block/workspace/block-workspace-editor.element.ts
@@ -5,34 +5,34 @@ import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 @customElement('umb-block-workspace-editor')
 export class UmbBlockWorkspaceEditorElement extends UmbLitElement {
 
-  @property({ type: String, attribute: false })
-  workspaceAlias?: string;
+	@property({ type: String, attribute: false })
+	workspaceAlias?: string;
 
-  @property({ type: String, attribute: false })
-  label?: string;
+	@property({ type: String, attribute: false })
+	label?: string;
 
-  render() {
-    return this.workspaceAlias && this.label
-      ? html` <umb-workspace-editor alias=${this.workspaceAlias} headline=${this.label}> </umb-workspace-editor> `
-      : '';
-  }
+	render() {
+		return this.workspaceAlias && this.label
+			? html` <umb-workspace-editor alias=${this.workspaceAlias} headline=${this.label}> </umb-workspace-editor> `
+			: '';
+	}
 
-  static styles = [
-    UmbTextStyles,
-    css`
+	static styles = [
+		UmbTextStyles,
+		css`
 			:host {
 				display: block;
 				width: 100%;
 				height: 100%;
 			}
 		`,
-  ];
+	];
 }
 
 export default UmbBlockWorkspaceEditorElement;
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'umb-block-workspace-editor': UmbBlockWorkspaceEditorElement;
-  }
+	interface HTMLElementTagNameMap {
+		'umb-block-workspace-editor': UmbBlockWorkspaceEditorElement;
+	}
 }
