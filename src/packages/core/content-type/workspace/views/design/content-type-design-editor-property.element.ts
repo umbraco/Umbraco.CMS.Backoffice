c490191dca6daab4bcedde8912fec2fab406520f
Lone Iversen <108085781+loivsen@users.noreply.github.com>
2024-07-10 12:44:33 +0200

    unused

--- src/packages/core/content-type/workspace/views/design/content-type-design-editor-property.element.ts
+++ src/packages/core/content-type/workspace/views/design/content-type-design-editor-property.element.ts
@@ -25,7 +25,6 @@ export class UmbContentTypeDesignEditorPropertyElement extends UmbLitElement {
 	//
 	#dataTypeDetailRepository = new UmbDataTypeDetailRepository(this);
 	#dataTypeUnique?: string;
-	#propertyUnique?: string;
 	#context = new UmbPropertyTypeContext(this);
 
 	@property({ attribute: false })
