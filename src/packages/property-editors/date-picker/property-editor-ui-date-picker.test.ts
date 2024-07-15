a3ed1a692ff3984af8dc7c2aba0b745fb895beb8
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-07-15 10:58:41 +0200

    test: adds tests for all the various input and output cases to ensure the format is 100% matching

--- src/packages/property-editors/date-picker/property-editor-ui-date-picker.test.ts
+++ src/packages/property-editors/date-picker/property-editor-ui-date-picker.test.ts
@@ -3,6 +3,7 @@ import { UmbPropertyEditorUIDatePickerElement } from './property-editor-ui-date-
 import { expect, fixture, html } from '@open-wc/testing';
 import { type UmbTestRunnerWindow, defaultA11yConfig } from '@umbraco-cms/internal/test-utils';
 import { UmbPropertyEditorConfigCollection } from '@umbraco-cms/backoffice/property-editor';
+import type { UUIInputElement } from '@umbraco-cms/backoffice/external/uui';
 
 describe('UmbPropertyEditorUIDatePickerElement', () => {
 	let element: UmbPropertyEditorUIDatePickerElement;
@@ -43,4 +44,57 @@ describe('UmbPropertyEditorUIDatePickerElement', () => {
 			await expect(element).shadowDom.to.be.accessible(defaultA11yConfig);
 		});
 	}
+
+	describe('input', () => {
+		it('should format the value to a datetime-local', async () => {
+			element.value = '2024-05-03 10:44:00';
+			await element.updateComplete;
+			expect(inputElement.value).to.equal('2024-05-03 10:44:00');
+		});
+
+		it('should format the value to a date', async () => {
+			element.value = '2024-05-03 10:44:00';
+			element.config = new UmbPropertyEditorConfigCollection([{ alias: 'format', value: 'YYYY-MM-dd' }]);
+			await element.updateComplete;
+			expect(inputElement.value).to.equal('2024-05-03');
+		});
+
+		it('should format the value to a time', async () => {
+			element.value = '2024-05-03 10:44:00';
+			element.config = new UmbPropertyEditorConfigCollection([{ alias: 'format', value: 'HH:mm' }]);
+			await element.updateComplete;
+			expect(inputElement.value).to.equal('10:44:00');
+		});
+	});
+
+	describe('output', () => {
+		let innerInput: UUIInputElement;
+
+		beforeEach(async () => {
+			innerInput = inputElement.shadowRoot?.querySelector('uui-input') as UUIInputElement;
+		});
+
+		it('should format the value to a datetime-local', async () => {
+			innerInput.value = '2024-05-03T10:44:00';
+			innerInput.dispatchEvent(new CustomEvent('change'));
+			await element.updateComplete;
+			expect(element.value).to.equal('2024-05-03 10:44:00');
+		});
+
+		it('should format the value to a date', async () => {
+			element.config = new UmbPropertyEditorConfigCollection([{ alias: 'format', value: 'YYYY-MM-dd' }]);
+			innerInput.value = '2024-05-03T10:44:00';
+			innerInput.dispatchEvent(new CustomEvent('change'));
+			await element.updateComplete;
+			expect(element.value).to.equal('2024-05-03');
+		});
+
+		it('should format the value to a time', async () => {
+			element.config = new UmbPropertyEditorConfigCollection([{ alias: 'format', value: 'HH:mm' }]);
+			innerInput.value = '2024-05-03T10:44:00';
+			innerInput.dispatchEvent(new CustomEvent('change'));
+			await element.updateComplete;
+			expect(element.value).to.equal('10:44:00');
+		});
+	});
 });
