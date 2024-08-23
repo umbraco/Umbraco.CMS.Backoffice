eb84b5bf892af9643de039fce2ad0eb13f5a16a2
Mads Rasmussen <madsr@hey.com>
2024-08-23 20:33:25 +0200

    register context

--- src/packages/user/active-users/manifests.ts
+++ src/packages/user/active-users/manifests.ts
@@ -1,5 +1,11 @@
 export const manifests = [
 	{
+		type: 'globalContext',
+		alias: 'Umb.GlobalContext.ActiveUsers',
+		name: 'Active Users Context',
+		api: () => import('./active-users.context.js'),
+	},
+	{
 		type: 'headerApp',
 		alias: 'Umb.HeaderApp.ActiveUsers',
 		name: 'Active Users Header App',
