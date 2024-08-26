66b1a32fdfcc613102c88b5daa9a4625d89d9cfa
Mads Rasmussen <madsr@hey.com>
2024-08-26 10:45:40 +0200

    simplify exports

--- src/packages/documents/document-types/components/index.ts
+++ src/packages/documents/document-types/components/index.ts
@@ -1,5 +1,1 @@
-export {
-	UmbDocumentTypePickerContext,
-	UmbDocumentTypePickerInputContext,
-} from './input-document-type/input-document-type.context.js';
-export { UmbInputDocumentTypeElement } from './input-document-type/input-document-type.element.js';
+export * from './input-document-type/index.js';
