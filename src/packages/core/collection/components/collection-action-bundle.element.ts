bc44e436eec1c1e0b22006a89db429d139aa8a30
Mads Rasmussen <madsr@hey.com>
2024-08-30 11:06:52 +0200

    Update collection-action-bundle.element.ts

--- src/packages/core/collection/components/collection-action-bundle.element.ts
+++ src/packages/core/collection/components/collection-action-bundle.element.ts
@@ -1,4 +1,5 @@
-import { html, customElement } from '@umbraco-cms/backoffice/external/lit';
+import type { CSSResultGroup } from '@umbraco-cms/backoffice/external/lit';
+import { html, customElement, css } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 
 @customElement('umb-collection-action-bundle')
@@ -6,6 +7,14 @@ export class UmbCollectionActionBundleElement extends UmbLitElement {
 	override render() {
 		return html`<umb-extension-slot type="collectionAction"></umb-extension-slot>`;
 	}
+
+	static override styles: CSSResultGroup = [
+		css`
+			:host {
+				display: contents;
+			}
+		`,
+	];
 }
 
 declare global {
