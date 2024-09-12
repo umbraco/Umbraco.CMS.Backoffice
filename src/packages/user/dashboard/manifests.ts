a2a994b002cc3dc1e9684033d357fa98a274f435
Mads Rasmussen <madsr@hey.com>
2024-09-12 20:30:30 +0200

    Create manifests.ts

---
+++ src/packages/user/dashboard/manifests.ts
@@ -0,0 +1,22 @@
+import { UMB_USER_MANAGEMENT_SECTION_ALIAS } from '../section/index.js';
+import type { ManifestTypes, UmbBackofficeManifestKind } from '@umbraco-cms/backoffice/extension-registry';
+
+export const manifests: Array<ManifestTypes | UmbBackofficeManifestKind> = [
+	{
+		type: 'dashboard',
+		kind: 'default',
+		alias: 'Umb.Dashboard.UserManagement.Overview',
+		name: 'User Management Overview Dashboard',
+		weight: 500,
+		meta: {
+			label: 'Overview',
+			pathname: 'overview',
+		},
+		conditions: [
+			{
+				alias: 'Umb.Condition.SectionAlias',
+				match: UMB_USER_MANAGEMENT_SECTION_ALIAS,
+			},
+		],
+	},
+];
