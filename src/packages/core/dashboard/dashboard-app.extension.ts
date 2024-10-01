6eda1e0f455065f784e3260069ed9f3439950a77
Mads Rasmussen <madsr@hey.com>
2024-10-01 20:23:38 +0200

    move

--- src/packages/core/dashboard/dashboard-app.extension.ts
+++ src/packages/core/dashboard/dashboard-app.extension.ts
@@ -1,6 +1,12 @@
-import type { ConditionTypes } from '../conditions/types.js';
+import type { ConditionTypes } from '../extension-registry/conditions/types.js';
 import type { ManifestElement, ManifestWithDynamicConditions } from '@umbraco-cms/backoffice/extension-api';
 
 export interface ManifestDashboardApp extends ManifestElement, ManifestWithDynamicConditions<ConditionTypes> {
 	type: 'dashboardApp';
+}
+
+declare global {
+	interface UmbExtensionManifestMap {
+		umbDashboardApp: ManifestDashboardApp;
+	}
 }
