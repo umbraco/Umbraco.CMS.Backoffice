de06477c7c6a06a8ab0caa42c04600fbc98069b4
Lone Iversen <108085781+loivsen@users.noreply.github.com>
2024-10-07 13:07:02 +0200

    Feature: Adds the missing dictionary to search

--- src/packages/dictionary/manifests.ts
+++ src/packages/dictionary/manifests.ts
@@ -4,6 +4,7 @@ import { manifests as entityActionManifests } from './entity-action/manifests.js
 import { manifests as menuItemManifests } from './menu-item/manifests.js';
 import { manifests as repositoryManifests } from './repository/manifests.js';
 import { manifests as treeManifests } from './tree/manifests.js';
+import { manifests as searchManifests } from './search/manifests.js';
 import { manifests as workspaceManifests } from './workspace/manifests.js';
 
 export const manifests: Array<UmbExtensionManifest> = [
@@ -13,5 +14,6 @@ export const manifests: Array<UmbExtensionManifest> = [
 	...menuItemManifests,
 	...repositoryManifests,
 	...treeManifests,
+	...searchManifests,
 	...workspaceManifests,
 ];
