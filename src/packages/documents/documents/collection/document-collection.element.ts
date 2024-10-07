61dd33a781fd07de680e6c87d7bfc6a333c49fe1
leekelleher <leekelleher@gmail.com>
2024-10-07 11:49:33 +0100

    Refactored Document collection toolbar
    Deprecates `umb-document-collection-toolbar`

--- src/packages/documents/documents/collection/document-collection.element.ts
+++ src/packages/documents/documents/collection/document-collection.element.ts
@@ -1,19 +1,58 @@
-import { html, customElement } from '@umbraco-cms/backoffice/external/lit';
-import { UmbCollectionDefaultElement } from '@umbraco-cms/backoffice/collection';
+import { css, customElement, html } from '@umbraco-cms/backoffice/external/lit';
+import { UmbCollectionDefaultElement, UMB_COLLECTION_CONTEXT } from '@umbraco-cms/backoffice/collection';
+import type { UmbDefaultCollectionContext } from '@umbraco-cms/backoffice/collection';
 
-import './document-collection-toolbar.element.js';
+const elementName = 'umb-document-collection';
 
-@customElement('umb-document-collection')
+@customElement(elementName)
 export class UmbDocumentCollectionElement extends UmbCollectionDefaultElement {
-	protected override renderToolbar() {
-		return html`<umb-document-collection-toolbar slot="header"></umb-document-collection-toolbar>`;
+	#collectionContext?: UmbDefaultCollectionContext;
+
+	#inputTimer?: NodeJS.Timeout;
+	#inputTimerAmount = 500;
+
+	constructor() {
+		super();
+
+		this.consumeContext(UMB_COLLECTION_CONTEXT, (context) => {
+			this.#collectionContext = context;
+		});
 	}
+
+	#onInput(event: InputEvent & { target: HTMLInputElement }) {
+		const filter = event.target.value ?? '';
+		clearTimeout(this.#inputTimer);
+		this.#inputTimer = setTimeout(() => this.#collectionContext?.setFilter({ filter }), this.#inputTimerAmount);
+	}
+
+	protected override renderToolbar() {
+		return html`
+			<umb-collection-toolbar slot="header">
+				<uui-input
+					id="input-search"
+					label=${this.localize.term('general_search')}
+					placeholder=${this.localize.term('placeholders_search')}
+					@input=${this.#onInput}></uui-input>
+			</umb-collection-toolbar>
+		`;
+	}
+
+	static override readonly styles = [
+		css`
+			#input-search {
+				display: block;
+			}
+		`,
+	];
 }
 
+/** @deprecated Should be exported as `element` only; to be removed in Umbraco 17. */
 export default UmbDocumentCollectionElement;
+
+export { UmbDocumentCollectionElement as element };
 
 declare global {
 	interface HTMLElementTagNameMap {
-		'umb-document-collection': UmbDocumentCollectionElement;
+		[elementName]: UmbDocumentCollectionElement;
 	}
 }
