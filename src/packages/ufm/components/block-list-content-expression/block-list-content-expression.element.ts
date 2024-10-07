5fc07cba6e5babea03fbef078e2f1ce4c4cbe0f6
Ibrahim Nada <ibrrahim.nada@gmail.com>
2024-10-07 19:54:03 +0300

    change let to const

--- src/packages/ufm/components/block-list-content-expression/block-list-content-expression.element.ts
+++ src/packages/ufm/components/block-list-content-expression/block-list-content-expression.element.ts
@@ -11,11 +11,11 @@ export class UmbBlockListContentExpressionElement extends UmbUfmElementBase {
 	constructor() {
 		super();
 		this.consumeContext(UMB_BLOCK_ENTRY_CONTEXT, async (context) => {
-			let content = await context.contentValues();
+			const content = await context.contentValues();
 			this.observe(
 				content,
 				(value) => {
-					let exprestionValue = this.getValueFromExpression(value, this.alias!);
+					const exprestionValue = this.getValueFromExpression(value, this.alias!);
 					if (exprestionValue === undefined) {
 						this.observe(context.contentElementTypeName, (value) => {
 							this.value = this.localize.term(value!);
