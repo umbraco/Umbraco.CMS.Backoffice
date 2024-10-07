5a1aabea2b5796605e1059bb7333fcd9dbee03f5
leekelleher <leekelleher@gmail.com>
2024-10-07 11:48:58 +0100

    Refactored Dictionary collection toolbar

--- src/packages/dictionary/collection/dictionary-collection.element.ts
+++ src/packages/dictionary/collection/dictionary-collection.element.ts
@@ -1,54 +1,55 @@
 import { css, customElement, html } from '@umbraco-cms/backoffice/external/lit';
-import {
-	UMB_COLLECTION_CONTEXT,
-	UmbCollectionDefaultElement,
-	type UmbDefaultCollectionContext,
-} from '@umbraco-cms/backoffice/collection';
+import { UmbCollectionDefaultElement, UMB_COLLECTION_CONTEXT } from '@umbraco-cms/backoffice/collection';
+import type { UmbDefaultCollectionContext } from '@umbraco-cms/backoffice/collection';
 
-@customElement('umb-dictionary-collection')
+const elementName = 'umb-dictionary-collection';
+
+@customElement(elementName)
 export class UmbDictionaryCollectionElement extends UmbCollectionDefaultElement {
 	#collectionContext?: UmbDefaultCollectionContext;
+
 	#inputTimer?: NodeJS.Timeout;
 	#inputTimerAmount = 500;
 
 	constructor() {
 		super();
+
 		this.consumeContext(UMB_COLLECTION_CONTEXT, (context) => {
 			this.#collectionContext = context;
 		});
 	}
 
-	#updateSearch(event: InputEvent) {
-		const target = event.target as HTMLInputElement;
-		const filter = target.value || '';
+	#onInput(event: InputEvent & { target: HTMLInputElement }) {
+		const filter = event.target.value ?? '';
 		clearTimeout(this.#inputTimer);
 		this.#inputTimer = setTimeout(() => this.#collectionContext?.setFilter({ filter }), this.#inputTimerAmount);
 	}
 
 	protected override renderToolbar() {
-		return html`<umb-collection-toolbar slot="header">${this.#renderSearch()}</umb-collection-toolbar>`;
+		return html`
+			<umb-collection-toolbar slot="header">
+				<uui-input
+					id="input-search"
+					label=${this.localize.term('general_search')}
+					placeholder=${this.localize.term('placeholders_search')}
+					@input=${this.#onInput}></uui-input>
+			</umb-collection-toolbar>
+		`;
 	}
 
-	#renderSearch() {
-		return html`<uui-input
-			id="input-search"
-			@input=${this.#updateSearch}
-			placeholder=${this.localize.term('placeholders_search')}></uui-input>`;
-	}
-
-	static override styles = [
+	static override readonly styles = [
 		css`
 			#input-search {
-				width: 100%;
+				display: block;
 			}
 		`,
 	];
 }
 
-export default UmbDictionaryCollectionElement;
+export { UmbDictionaryCollectionElement as element };
 
 declare global {
 	interface HTMLElementTagNameMap {
-		'umb-dictionary-collection': UmbDictionaryCollectionElement;
+		[elementName]: UmbDictionaryCollectionElement;
 	}
 }
