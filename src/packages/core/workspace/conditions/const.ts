56b1dcc1c18311b833aa794ea0cbaec7f7bbd251
Warren Buckley <warren@hackmakedo.com>
2024-10-14 09:16:03 +0100

    Add a const for 'Umb.Condition.WorkspaceAlias'

--- src/packages/core/workspace/conditions/const.ts
+++ src/packages/core/workspace/conditions/const.ts
@@ -1,2 +1,3 @@
 export const UMB_WORKSPACE_HAS_COLLECTION_CONDITION = 'Umb.Condition.WorkspaceHasCollection';
 export const UMB_WORKSPACE_ENTITY_IS_NEW_CONDITION = 'Umb.Condition.WorkspaceEntityIsNew';
+export const UMB_WORKSPACE_ALIAS_CONDITION = 'Umb.Condition.WorkspaceAlias';
