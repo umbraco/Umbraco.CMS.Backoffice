5c0c402cfa24a8a72d22fb46b8c64e4b3fd13a44
Mads Rasmussen <madsr@hey.com>
2024-10-22 13:51:05 +0200

    set pending changes value in an observable

--- src/packages/documents/documents/publishing/publish-pending-changes.manager.ts
+++ src/packages/documents/documents/publishing/publish-pending-changes.manager.ts
@@ -3,37 +3,63 @@ import type { UmbDocumentDetailModel } from '../types.js';
 import { UmbVariantId } from '@umbraco-cms/backoffice/variant';
 import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
 import { UmbMergeContentVariantDataController } from '@umbraco-cms/backoffice/content';
-import { jsonStringComparison } from '@umbraco-cms/backoffice/observable-api';
+import { jsonStringComparison, UmbArrayState } from '@umbraco-cms/backoffice/observable-api';
+
+interface UmbPublishPendingChangesManagerProcessArgs {
+	unique: string;
+	currentData: UmbDocumentDetailModel;
+}
+
+interface VariantPendingChange {
+	variantId: UmbVariantId;
+}
 
 export class UmbPublishPendingChangesManager extends UmbControllerBase {
+	#pendingChanges = new UmbArrayState<VariantPendingChange>([], (x) => x.variantId.toString());
+	public readonly = this.#pendingChanges.asObservable();
+
 	#publishingRepository = new UmbDocumentPublishingRepository(this);
 
-	async process({ unique, currentData }: { unique: string; currentData: UmbDocumentDetailModel }) {
-		if (!unique) throw new Error('Unique is missing');
-		if (!currentData) throw new Error('Current Data is missing');
+	/**
+	 * Checks each variant if there are any pending changes to publish.
+	 * @param {UmbPublishPendingChangesManagerProcessArgs} args - The arguments for the process.
+	 * @param {string} args.unique - The unique identifier of the document.
+	 * @param {UmbDocumentDetailModel} args.currentData - The current document data.
+	 * @returns {Promise<void>}
+	 * @memberof UmbPublishPendingChangesManager
+	 */
+	async process(args: UmbPublishPendingChangesManagerProcessArgs): Promise<void> {
+		if (!args.unique) throw new Error('Unique is missing');
+		if (!args.currentData) throw new Error('Current Data is missing');
 
-		const { data: publishedData } = await this.#publishingRepository.published(unique);
+		const { data: publishedData } = await this.#publishingRepository.published(args.unique);
 
-		if (!publishedData) return;
+		if (!publishedData) {
+			this.#pendingChanges.setValue([]);
+			return;
+		}
 
-		const variantIds = currentData.variants?.map((x) => UmbVariantId.Create(x)) ?? [];
+		const variantIds = args.currentData.variants?.map((x) => UmbVariantId.Create(x)) ?? [];
 
-		variantIds.forEach(async (variantId) => {
+		const pendingChangesPromises = variantIds.map(async (variantId) => {
 			const mergedData = await new UmbMergeContentVariantDataController(this).process(
 				publishedData,
-				currentData,
+				args.currentData,
 				[variantId],
 				[variantId],
 			);
 
-			console.log('--------------');
-			console.log('Variant', variantId.culture);
-			console.log('Merged data', mergedData.values);
-			console.log('Published data', publishedData.values);
-
-			if (jsonStringComparison(mergedData.values, publishedData.values) === false) {
-				console.log('This variant has changes', variantId.culture);
+			if (jsonStringComparison(mergedData, publishedData) === false) {
+				return { variantId };
+			} else {
+				return null;
 			}
 		});
+
+		const variantsWithPendingChanges = (await Promise.all(pendingChangesPromises)).filter(
+			(x) => x !== null,
+		) as VariantPendingChange[];
+
+		this.#pendingChanges.setValue(variantsWithPendingChanges);
 	}
 }
