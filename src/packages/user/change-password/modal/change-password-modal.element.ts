90b166c6029c6b274cd4fee2dae164d48b9f2e68
leekelleher <leekelleher@gmail.com>
2024-11-06 17:10:42 +0000

    Refactored to use built-in validation
    Wires up most localization keys.

--- src/packages/user/change-password/modal/change-password-modal.element.ts
+++ src/packages/user/change-password/modal/change-password-modal.element.ts
@@ -1,24 +1,27 @@
 import type { UmbChangePasswordModalData, UmbChangePasswordModalValue } from './change-password-modal.token.js';
-import { UMB_CURRENT_USER_CONTEXT } from '@umbraco-cms/backoffice/current-user';
-import { UmbUserItemRepository } from '@umbraco-cms/backoffice/user';
-import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
-import type { CSSResultGroup } from '@umbraco-cms/backoffice/external/lit';
-import { css, html, nothing, customElement, state } from '@umbraco-cms/backoffice/external/lit';
+import { css, customElement, html, query, state, when } from '@umbraco-cms/backoffice/external/lit';
 import { UmbModalBaseElement } from '@umbraco-cms/backoffice/modal';
+import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
+import { UmbUserItemRepository } from '@umbraco-cms/backoffice/user';
+import { UMB_CURRENT_USER_CONTEXT } from '@umbraco-cms/backoffice/current-user';
+import type { UUIInputPasswordElement } from '@umbraco-cms/backoffice/external/uui';
 
 @customElement('umb-change-password-modal')
 export class UmbChangePasswordModalElement extends UmbModalBaseElement<
 	UmbChangePasswordModalData,
 	UmbChangePasswordModalValue
 > {
+	@query('#newPassword')
+	private _newPasswordInput?: UUIInputPasswordElement;
+
+	@query('#confirmPassword')
+	private _confirmPasswordInput?: UUIInputPasswordElement;
+
 	@state()
-	private _headline: string = 'Change password';
+	private _headline: string = this.localize.term('general_changePassword');
 
 	@state()
 	private _isCurrentUser: boolean = false;
-
-	@state()
-	private _isMatchedPassword: boolean = true;
 
 	#userItemRepository = new UmbUserItemRepository(this);
 	#currentUserContext?: typeof UMB_CURRENT_USER_CONTEXT.TYPE;
@@ -38,18 +41,8 @@ export class UmbChangePasswordModalElement extends UmbModalBaseElement<
 
 		const formData = new FormData(form);
 
-		// TODO: validate that the new password and confirm password match
 		const oldPassword = formData.get('oldPassword') as string;
 		const newPassword = formData.get('newPassword') as string;
-		const confirmPassword = formData.get('confirmPassword') as string;
-
-		if(confirmPassword == newPassword){
-			this._isMatchedPassword = true;
-		}
-		else{
-			this._isMatchedPassword = false;
-			return;
-		}
 
 		this.value = { oldPassword, newPassword };
 		this.modalContext?.submit();
@@ -65,12 +58,7 @@ export class UmbChangePasswordModalElement extends UmbModalBaseElement<
 	}
 
 	async #setIsCurrentUser() {
-		if (!this.data?.user.unique) {
-			this._isCurrentUser = false;
-			return;
-		}
-
-		if (!this.#currentUserContext) {
+		if (!this.#currentUserContext || !this.data?.user.unique) {
 			this._isCurrentUser = false;
 			return;
 		}
@@ -78,7 +66,13 @@ export class UmbChangePasswordModalElement extends UmbModalBaseElement<
 		this._isCurrentUser = await this.#currentUserContext.isUserCurrentUser(this.data.user.unique);
 	}
 
-	protected override async firstUpdated(): Promise<void> {
+	protected override async firstUpdated() {
+		this._confirmPasswordInput?.addValidator(
+			'customError',
+			() => this.localize.term('user_passwordMismatch'),
+			() => this._confirmPasswordInput?.value !== this._newPasswordInput?.value,
+		);
+
 		if (!this.data?.user.unique) return;
 		const { data } = await this.#userItemRepository.requestItems([this.data.user.unique]);
 
@@ -88,73 +82,68 @@ export class UmbChangePasswordModalElement extends UmbModalBaseElement<
 		}
 	}
 
-	#onPasswordUpdate(){
-		this._isMatchedPassword = true;
-	}
-
 	override render() {
 		return html`
 			<uui-dialog-layout class="uui-text" headline=${this._headline}>
 				<uui-form>
-					<form id="ChangePasswordForm" @submit="${this.#onSubmit}">
-						${this._isCurrentUser ? this.#renderOldPasswordInput() : nothing}
+					<form id="ChangePasswordForm" @submit=${this.#onSubmit}>
+						${when(
+							this._isCurrentUser,
+							() => html`
+								<uui-form-layout-item style="margin-bottom: var(--uui-size-layout-2)">
+									<uui-label slot="label" id="oldPasswordLabel" for="oldPassword" required>
+										<umb-localize key="user_passwordCurrent">Current password</umb-localize>
+									</uui-label>
+									<uui-input-password
+										id="oldPassword"
+										name="oldPassword"
+										required
+										required-message="Current password is required">
+									</uui-input-password>
+								</uui-form-layout-item>
+							`,
+						)}
 						<uui-form-layout-item>
-							<uui-label id="newPasswordLabel" for="newPassword" slot="label" required>New password</uui-label>
+							<uui-label slot="label" id="newPasswordLabel" for="newPassword" required>
+								<umb-localize key="user_newPassword">New password</umb-localize>
+							</uui-label>
 							<uui-input-password
 								id="newPassword"
 								name="newPassword"
 								required
-								required-message="New password is required"></uui-input-password>
+								required-message="New password is required">
+							</uui-input-password>
 						</uui-form-layout-item>
 						<uui-form-layout-item>
-							<uui-label id="confirmPasswordLabel" for="confirmPassword" slot="label" required
-								>Confirm password</uui-label
-							>
+							<uui-label slot="label" id="confirmPasswordLabel" for="confirmPassword" required>
+								<umb-localize key="user_confirmNewPassword">Confirm new password</umb-localize>
+							</uui-label>
 							<uui-input-password
 								id="confirmPassword"
 								name="confirmPassword"
 								required
-								required-message="Confirm password is required"
-								@input=${() => this.#onPasswordUpdate()}></uui-input-password>
+								required-message="Confirm password is required">
+							</uui-input-password>
 						</uui-form-layout-item>
-						${this._isMatchedPassword ? nothing: html `<span class="validation-error">The confirmed password doesn't match the new password!</span>`}
 					</form>
 				</uui-form>
-
-				<uui-button slot="actions" @click=${this.#onClose} label="Cancel"></uui-button>
+				<uui-button slot="actions" label=${this.localize.term('general_cancel')} @click=${this.#onClose}></uui-button>
 				<uui-button
 					slot="actions"
 					type="submit"
-					label="Confirm"
-					look="primary"
+					form="ChangePasswordForm"
 					color="positive"
-					form="ChangePasswordForm"></uui-button>
+					look="primary"
+					label=${this.localize.term('general_confirm')}></uui-button>
 			</uui-dialog-layout>
 		`;
 	}
 
-	#renderOldPasswordInput() {
-		return html`
-			<uui-form-layout-item style="margin-bottom: var(--uui-size-layout-2)">
-				<uui-label id="oldPasswordLabel" for="oldPassword" slot="label" required>Old password</uui-label>
-				<uui-input-password
-					id="oldPassword"
-					name="oldPassword"
-					required
-					required-message="Old password is required"></uui-input-password>
-			</uui-form-layout-item>
-		`;
-	}
-
-	static override styles: CSSResultGroup = [
+	static override readonly styles = [
 		UmbTextStyles,
 		css`
 			uui-input-password {
 				width: 100%;
-			}
-			.validation-error {
-				margin-top: 0 !important;
-				color: var(--uui-color-danger);
 			}
 		`,
 	];
